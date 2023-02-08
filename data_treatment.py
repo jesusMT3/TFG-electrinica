7d91437a08a26ccf14a270a695a3f81cf88704e0
jesusMT3 <117745845+jesusMT3@users.noreply.github.com>
2023-02-08 18:20:28 +0100

    Insolation Cell
    New cell to obtain insolation measures and color grid of the surface of the model

--- data_treatment.py
+++ data_treatment.py
@@ -53,38 +53,45 @@ for i in range(1, 19):
     except KeyError:
         print("Channel ",i ," does not exist")
         
-#%% 
+#%% # Get irradiance from voltage data
 
-# Get irradiance from voltage data
 filtered_data['T_av'] = filtered_data[['CH19', 'CH20']].mean(axis=1) #average temperature
 k= [0.1658, 0.1638, 0.1664, 0.1678, 0.3334, 0.1686, 0.1673, inf, inf, inf, inf, 0.3306, 0.3317, 0.3341, 0.3361]
 alpha = 4.522e-4 # pu units
 T0  = 298.15 # STC temperature
 
+for i in range(1, 19):
+    # Irradiance conversion with temperature dependance
+    try:
+        coef = 1 + alpha * ((filtered_data['T_av'] + 273.15)- T0)
+        filtered_data['W' +  str(i)] = filtered_data["CH" + str(i)] / coef
+        filtered_data['W' +  str(i)] /= k[i-1]
+        print(k[i-1])
+    except KeyError:
+        continue
+
+#%% Plottings
+
 # East plate
 
-for i in range(1, 5):
-    # Irradiance conversion with temperature dependance
-    coef = 1 + alpha * ((filtered_data['T_av'] + 273.15)- T0)
-    aux_str = "CH" + str(i)
-    filtered_data['W' +  str(i)] = filtered_data[aux_str] / (coef * k[i - 1])
-    filtered_data['W' +  str(i)].plot(linewidth = 1)
-    
 
+plt.figure(num = 1)
+
+for i in range (1,5):
+    filtered_data['W' + str(i)].plot(linewidth = 1)
+    
 plt.xlabel('Date Time')
 plt.ylabel('Irradiance [W/m$^2$]')
 plt.title("East plate from " + filter_day)
 plt.legend()
 plt.grid(True)
 
-#%% North West Plate
-for i in range(5, 9):
-    # Irradiance conversion with temperature dependance
-    coef = 1 + alpha * ((filtered_data['T_av'] + 273.15)- T0)
-    aux_str = "CH" + str(i)
-    filtered_data['W' +  str(i)] = filtered_data[aux_str] / (coef * k[i - 1])
-    filtered_data['W' +  str(i)].plot(linewidth = 1)
-    
+# North West Plate
+
+plt.figure(num = 2)
+
+for i in range (5,9):
+    filtered_data['W' + str(i)].plot(linewidth = 1)
 
 plt.xlabel('Date Time')
 plt.ylabel('Irradiance [W/m$^2$]')
@@ -92,14 +99,11 @@ plt.title("North-West plate from " + filter_day)
 plt.legend()
 plt.grid(True)
 
-#%% North East Plate
-for i in range(11, 15):
-    # Irradiance conversion with temperature dependance
-    coef = 1 + alpha * ((filtered_data['T_av'] + 273.15)- T0)
-    aux_str = "CH" + str(i)
-    filtered_data['W' +  str(i)] = filtered_data[aux_str] / (coef * k[i - 1])
-    filtered_data['W' +  str(i)].plot(linewidth = 1)
-    
+# North East Plate
+plt.figure(num = 3)
+
+for i in range (11,15):
+    filtered_data['W' + str(i)].plot(linewidth = 1)
 
 plt.xlabel('Date Time')
 plt.ylabel('Irradiance [W/m$^2$]')
@@ -107,7 +111,8 @@ plt.title("South-West plate from " + filter_day)
 plt.legend()
 plt.grid(True)
 
-#%% Comparison between plates
+# Comparison between plates
+plt.figure(num = 4)
 filtered_data['e_average'] = filtered_data[['W1', 'W2', 'W3', 'W4']].mean(axis=1)   
 filtered_data['e_average'].plot(label = 'East', linewidth = 1.5)
 
@@ -122,3 +127,40 @@ plt.ylabel('Irradiance [W/m$^2$]')
 plt.title("Average irradiance from " + filter_day)
 plt.legend()
 plt.grid(True)
+
+#%% Daily insolation matrix
+
+dt = 1
+matrix = [[5, 5, 0, 0, 11, 11],
+          [0, 0, 0, 0, 0 , 0 ],
+          [0, 0, 0, 0, 0 , 0 ],
+          [0, 6, 0, 0, 12, 12],
+          [7, 0, 0, 0, 13, 13],
+          [0, 0, 0, 0, 0 , 0 ],
+          [0, 0, 0, 0, 0 , 0 ],
+          [8, 8, 0, 0, 14, 14],
+          [0, 0, 0, 4, 0 , 0 ],
+          [0, 0, 0, 0, 0 , 0 ],
+          [0, 0, 0, 0, 0 , 0 ],
+          [0, 0, 0, 3, 0 , 0 ],
+          [0, 0, 0, 2, 0 , 0 ],
+          [0, 0, 0, 0, 0 , 0 ],
+          [0, 0, 0, 0, 0 , 0 ],
+          [0, 0, 0, 1, 0 , 0 ]]
+
+
+insolation2d = np.zeros([len(matrix), len(matrix[0])])
+#get insolation matrix
+for i in range (0, len(matrix)):
+    for j in range (0, len(matrix[0])):
+        var = matrix[i][j]
+        try:
+            insolation2d[i][j] = filtered_data['CH' + str(var)].sum() / 3600
+        except KeyError:
+            insolation2d[i][j] = np.nan
+            
+
+
+plt.imshow(insolation2d, extent=[0, 42, 33, 0]) #sensor 
+plt.title('Model insolation map')
+plt.colorbar()
