e9856d10e85f1525891e94ad0d5a02446b1c5c05
jesusMT3 <117745845+jesusMT3@users.noreply.github.com>
2023-02-24 14:09:00 +0100

    Pmp function loop
    a module iterating through data to get Pmp value at all hours, incredibly slow, uncomfortable for 86400 length data

--- array_power.py
+++ array_power.py
@@ -29,13 +29,13 @@ plate1 = pd.DataFrame()
 plate_front = filtered_data['W15']
 for i in range (0, m):
     if i == 0:
-        plate1[i] = filtered_data['W1']
+        plate1[i] = filtered_data['W1'] + plate_front
     elif i == (m / 2) - 1:
-        plate1[i] = filtered_data['W2']
+        plate1[i] = filtered_data['W2'] + plate_front
     elif i == (m / 2) + 1:
-        plate1[i] = filtered_data['W3']
+        plate1[i] = filtered_data['W3'] + plate_front
     elif i == m - 1:
-        plate1[i] = filtered_data['W4']
+        plate1[i] = filtered_data['W4'] + plate_front
     else:
         plate1[i] = np.empty(len(filtered_data)) * np.nan
 
@@ -51,29 +51,35 @@ f.plot_channels(magnitude = 'Irradiance [W/m$^2$]', dataframe = plate1,
                 plate = plate1, 
                 title = 'East plate global Irradiance')
 #%%
-pvsys = pvsystem.PVsystem(numberStrs=1, numberMods=1)
-pmp = np.zeros(86400)
+import numpy as np
+import pvmismatch as pvm
 
-name = plate1.index[50000]
-data = plate1.loc[name]
-pannel = np.zeros([m,n])
-locations = np.zeros([m,n])
-z = 0
-for i in range (0, n):
-    if i % 2 != 0:
-        z += m - 1
-    for j in range (0, m):
-        pannel[j][i] = data[j]
-        locations[j][i] = z
-        if i % 2 != 0:
-            z -= 1
-        if i % 2 == 0:
-            z += 1
-        
+n = 8
+m = 12
 
-pvsys.setSuns({0: {0: [data/1000, pannel}})
+module = PVmodule()
+power = np.zeros(86400)
+for x in range (50000, 60000):
+    name = plate1.index[x]
+    data = plate1.loc[name]
+    dic = {}
+    suns = np.zeros(m*n)
+    z = 0
+    
+    for i in range(0, n):
+        if i != 0:
+            if i % 2 == 0:
+                z += m + 1
+            elif i % 2 != 0:
+                z += m - 1
+        for j in range(0, m):
+            suns[z] = data[j]
+            if i % 2 != 0:
+                z -= 1
+            if i % 2 == 0:
+                z += 1
+    
+    module.setSuns(suns/1000)
+    power[x] = module.Pmod.max()
 
-power = pvsys.Pmp
-
-plt.ion()
-func =  pvsys.plotSys()
+plt.plot(power)
