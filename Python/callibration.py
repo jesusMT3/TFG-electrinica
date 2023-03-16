cba1f29b9dd8bde8d2b1b8d350c928a49af2adda
jesusMT3 <117745845+jesusMT3@users.noreply.github.com>
2023-03-16 13:25:18 +0100

    callibration program
    The callibration program is ready to use

--- Python/callibration.py
+++ Python/callibration.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar  7 12:10:36 2023
-
+Program which calculates automatically irradiance/voltage coefficients
+based on linnear regression algorithms
 @author: Jesus
 """
 
@@ -12,22 +13,22 @@ import pandas as pd
 import scipy
 
 df = pd.DataFrame()
-results = {}
-plate = ['CH1', 'CH2', 'CH3', 'CH4']
-# plate = ['CH5', 'CH6', 'CH7', 'CH8']
+
+plate = ['CH1', 'CH2', 'CH3', 'CH4', 'CH5', 'CH6', 'CH7', 'CH8']
 
 def main():
     global datalogger_data
     global meteodata
     global df
     global data
+    global coefficients
     
     #import data from datalogger and meteodata station
     datalogger_data = dl.data_import('datalogger')
     meteodata = dl.data_import('meteodata')
     
     # if datalogger data is in seconds, uncomment this line
-    datalogger_data = datalogger_data.resample('T').mean()
+    # datalogger_data = datalogger_data.resample('T').mean()
     
     # create dataframe with requested data
     for i in plate:
@@ -39,6 +40,7 @@ def main():
     #temperature correction
     alpha = 4.522e-4 # pu units
     T0  = 25 # STC temperature
+    
     for i in plate:
         df[i] /= (1 + alpha * (df['Temp'] - T0))
         
@@ -46,7 +48,7 @@ def main():
     df.index = df['GHI']
     
     dl.plot_channels(magnitude = 'Vshunt[V]', dataframe = df, 
-                     plate = plate, title = 'Plate BE', xaxis = 'Irradiance [W/m$^2$]')
+                      plate = plate, title = 'Callibration data', xaxis = 'Irradiance [W/m$^2$]')
     
     #linear regression to obtain coefficients
     for i in plate:
